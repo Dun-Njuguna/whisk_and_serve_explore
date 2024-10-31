4000a2d5f9b02cd9a3266e606999686a3c8416c7
Duncan Kariuki <duncankariuki60@gmail.com>
2024-10-31 14:38:48 +0300

    Refactor Explore screen to maintain scroll state and improve error handling

--- lib/presentation/pages/explore.dart
+++ lib/presentation/pages/explore.dart
@@ -10,50 +10,69 @@ class Explore extends StatefulWidget {
   ExploreState createState() => ExploreState();
 }
 
-class ExploreState extends State<Explore> {
+class ExploreState extends State<Explore> with AutomaticKeepAliveClientMixin<Explore> {
+
+  @override
+  bool get wantKeepAlive => true;
+  
   @override
   void initState() {
     super.initState();
-    addBlocEvent<RecipeCategoriesBloc>(context, FetchRecipeCategories());
+    final state = getCurrentState<RecipeCategoriesBloc, RecipeCategoriesState>(context);
+
+    if (state is RecipeCategoriesInitial) {
+      addBlocEvent<RecipeCategoriesBloc>(context, FetchRecipeCategories());
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
+
     return Scaffold(
       body: createBlocBuilder<RecipeCategoriesBloc, RecipeCategoriesState>(
         builder: (context, state) {
-                      print("object.............. $state");
           if (state is RecipeCategoriesLoading) {
             return const Center(child: CircularProgressIndicator());
           } else if (state is RecipeCategoriesLoaded) {
-            return LayoutBuilder(
-              builder: (context, constraints) {
-                int columns = getColumnCount(screenWidth: constraints.maxWidth);
-                return GridView.builder(
-                  itemCount: state.categories.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: columns,
-                    crossAxisSpacing: 8,
-                    mainAxisSpacing: 4,
-                  ),
-                  padding: const EdgeInsets.all(8.0),
-                  itemBuilder: (context, index) {
-                    final category = state.categories[index];
-                    return CategoryItem(category: category);
-                  },
-                );
-              },
-            );
+            return _buildGridView(state);
           } else if (state is RecipeCategoriesError) {
-            SnackbarHandler(context).show(GenericMessage(
-              message: state.message,
-              type: MessageType.error,
-            ));
+            _showErrorSnackbar(state.message);
             return Center(child: Text(state.message));
           }
+
           return const Center(child: Text("Press refresh to load categories"));
         },
       ),
     );
+  }
+
+  Widget _buildGridView(RecipeCategoriesLoaded state) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        int columns = getColumnCount(screenWidth: constraints.maxWidth);
+        return GridView.builder(
+          key: const PageStorageKey<String>('exploreGrid'),
+          itemCount: state.categories.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: columns,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 4,
+          ),
+          padding: const EdgeInsets.all(8.0),
+          itemBuilder: (context, index) {
+            final category = state.categories[index];
+            return CategoryItem(category: category);
+          },
+        );
+      },
+    );
+  }
+
+  void _showErrorSnackbar(String message) {
+    SnackbarHandler(context).show(GenericMessage(
+      message: message,
+      type: MessageType.error,
+    ));
   }
 }
