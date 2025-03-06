5ee2b4b89726299f2d5a4a9a91d02cd0016f980b
Duncan Kariuki <duncankariuki60@gmail.com>
2025-03-06 06:58:18 +0300

    Allow adding a meal to favourites

--- lib/features/meal_details/presentation/pages/meal_details.dart
+++ lib/features/meal_details/presentation/pages/meal_details.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:whisk_and_serve_core/whisk_and_serve_core.dart';
 import 'package:whisk_and_serve_core/widgets/base_scaffold.dart';
@@ -13,41 +14,91 @@ class MealDetailsPage extends StatefulWidget {
 
 class _MealDetailsPageState extends State<MealDetailsPage> {
   String? mealId;
-
+  bool isFavourite = false;
   bool isInitialized = false;
+
+  MealDetails? meal;
+
+  StreamSubscription<FavouritesBusResponse>? _favouriteEventSubscription;
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
 
-    final newCategoryName =
-        NavigationHelper.getPathParameter(context, 'mealId');
+    final newMealId = NavigationHelper.getPathParameter(context, 'mealId');
 
-    if (mealId == newCategoryName && isInitialized) return;
+    // Avoid unnecessary re-initialization
+    if (mealId == newMealId && isInitialized) return;
 
-    mealId = newCategoryName;
+    mealId = newMealId;
     isInitialized = true;
 
+    // Fetch meal details
     addBlocEvent<MealDetailsBloc>(
       context,
       FetchMealDetailsById(mealId: mealId!),
     );
+
+    // Reset listeners to avoid duplication
+    _setupFavouriteListeners();
+
+    // Query favourite status for the meal
+    _queryFavouriteStatus();
+  }
+
+  void _setupFavouriteListeners() {
+    // Cancel existing subscriptions if any
+    _favouriteEventSubscription?.cancel();
+
+    // Listen to add/remove favourite events
+    _favouriteEventSubscription =
+        GlobalEventBus.on<FavouritesBusResponse>().listen((event) {
+      if (event.mealId == mealId) {
+        setState(() {
+          isFavourite = event.isFavourite;
+        });
+      }
+    });
+  }
+
+  void _queryFavouriteStatus() {
+    GlobalEventBus.trigger(
+      FavouritesBusQueryEvent(mealId: mealId!, action: FavouritesAction.query),
+    );
+  }
+
+  void _updateFavourite(MealDetails meal, FavouritesAction action) {
+    GlobalEventBus.trigger(
+      FavouritesBusEvent(meal: meal, action: action),
+    );
+  }
+
+  @override
+  void dispose() {
+    _favouriteEventSubscription?.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
       title: "",
+      isMealFavourite: isFavourite,
+      updateFavouriteStatus: () {
+        if (isFavourite && meal != null) {
+          _updateFavourite(meal!, FavouritesAction.remove);
+        } else if (!isFavourite && meal != null) {
+          _updateFavourite(meal!, FavouritesAction.add);
+        }
+      },
       child: createBlocBuilder<MealDetailsBloc, MealDetailsState>(
         builder: (context, state) {
           if (state is MealDetailsLoading) {
             return const Center(child: CircularProgressIndicator());
           } else if (state is MealDetailsLoaded) {
-            final meal = state.meal;
+            meal = state.meal;
             return SingleChildScrollView(
-              padding: const EdgeInsets.only(
-                bottom: 16.0,
-              ),
+              padding: const EdgeInsets.only(bottom: 16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -59,15 +110,13 @@ class _MealDetailsPageState extends State<MealDetailsPage> {
                           : MediaQuery.of(context).size.width,
                     ),
                     child: Image.network(
-                      meal.thumbnail,
+                      meal!.thumbnail,
                       fit: BoxFit.cover,
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(16.0),
-                    child: MealDetailsSection(
-                      meal: meal,
-                    ),
+                    child: MealDetailsSection(meal: meal!),
                   ),
                 ],
               ),
